18c5854fde0921c25e721f211904da40284ddad7
Peter-G-Stone <peter.stone.online@gmail.com>
2019-02-28 16:46:15 -0500

    login redirects you now, and uses an authAction, and gives window alert

--- photo-feed-react/src/components/LoginPage.js
+++ photo-feed-react/src/components/LoginPage.js
@@ -1,7 +1,11 @@
-import React, { Component } from 'react';
+// import { withRouter } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Form, Button, Col, Grid } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { authenticate } from '../actions/authActions'
 
 
-export default class LoginPage extends Component {
+class LoginPage extends Component {
     constructor(props) {
         super(props);
 
@@ -20,25 +24,32 @@ export default class LoginPage extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
 
-        const email = this.state.email 
-        const password = this.state.password
-        const request = JSON.stringify({'auth': {'email': email, 'password': password}})
+        if (this.props.authenticate(this.state)) {
+            this.props.history.push('/')
+            window.alert("Successfully logged in.")
+        } else {
+            window.alert("Sorry, something went wrong. Please try logging in again.")
+        }
 
-        console.log('submitting ', request)
-        fetch('http://localhost:3001/api/user_token',
-        {
-            method: "POST",
-            body: request,
-            headers: {
-                "Content-Type": "application/json"            }
-        })
-        .then(resp => {
-            return resp.json()
-        })
-        .then(data => {
-            localStorage.setItem("jwt", data.jwt)
-            console.log('data: ', data)
-        })
+        // const email = this.state.email 
+        // const password = this.state.password
+        // const request = JSON.stringify({'auth': {'email': email, 'password': password}})
+
+        // console.log('submitting ', request)
+        // fetch('http://localhost:3001/api/user_token',
+        // {
+        //     method: "POST",
+        //     body: request,
+        //     headers: {
+        //         "Content-Type": "application/json"            }
+        // })
+        // .then(resp => {
+        //     return resp.json()
+        // })
+        // .then(data => {
+        //     localStorage.setItem("jwt", data.jwt)
+        //     console.log('data: ', data)
+        // })
     }
 
     render() {
@@ -54,3 +65,7 @@ export default class LoginPage extends Component {
 
        )}
 }
+
+export default connect(null, {authenticate})(LoginPage)
+// the above had in Christine Tran's (as well as the commented out import line at beginning of file): 
+// export default Login = withRouter(connect(null, {authenticate})(Login));
