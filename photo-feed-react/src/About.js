cf3c1f7d9afbbb1db8e8d0a6c2034f6b4940d4c0
Peter-G-Stone <peter.stone.online@gmail.com>
2019-03-07 15:21:30 -0500

    formatting about component

--- photo-feed-react/src/About.js
+++ photo-feed-react/src/About.js
@@ -4,7 +4,11 @@ const About = () => {
     return (
         <>
             <p>This is a personal project by Peter Stone.</p>
-            <p><a href="https://www.linkedin.com/in/peter-stone-0017a691/">LinkedIn</a> | <a href="mailto:peter.stone.online@gmail.com">Email</a> | <a href="https://github.com/peter-g-stone/">GitHub</a></p>
+            <p>
+                <a href="https://www.linkedin.com/in/peter-stone-0017a691/">LinkedIn</a> | 
+                <a href="mailto:peter.stone.online@gmail.com">Email</a> | 
+                <a href="https://github.com/peter-g-stone/">GitHub</a>
+            </p>
         </>
     )
 }
