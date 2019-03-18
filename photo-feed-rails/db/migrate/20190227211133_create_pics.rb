05ae71a19dbdaf50a444cf41eb8c9611f4aada68
Peter-G-Stone <peter.stone.online@gmail.com>
2019-03-17 22:19:53 -0400

    change migration so pics have int for likes

--- photo-feed-rails/db/migrate/20190227211133_create_pics.rb
+++ photo-feed-rails/db/migrate/20190227211133_create_pics.rb
@@ -2,6 +2,7 @@ class CreatePics < ActiveRecord::Migration[5.2]
   def change
     create_table :pics do |t|
       t.string :url
+      t.integer :likes, :default => 0, :null => false
       t.belongs_to :artist, index: true
       t.timestamps
     end
